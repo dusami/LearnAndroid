e11d837a3b056c44bfe5cfd37449c63e69f2fee0
zjzj_cjq <zjzj_cjq@gmail.com>
2019-07-15 20:41:31 +0800

    Code changed

--- app/src/main/java/com/example/myapplication/Service/MyService.java
+++ app/src/main/java/com/example/myapplication/Service/MyService.java
@@ -1,10 +1,20 @@
 package com.example.myapplication.Service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.myapplication.R;
 
 public class MyService extends Service {
 
@@ -24,6 +34,23 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate: executed");
+
+        Intent intent = new Intent(this, ServiceActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0 , intent, 0);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel("Channel_service", "service", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(notificationChannel);
+        }
+        Notification notification = new NotificationCompat.Builder(this, "Channel_service")
+                .setContentTitle("This is content titile")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(2, notification);
     }
 
     @Override
